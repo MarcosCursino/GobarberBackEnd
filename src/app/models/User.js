81b3753f064e3df431419769661478c951fde2e9
Marcos <marcos.cursino@hotmail.com>
2020-02-27 22:40:11 -0300

    upload de arquivos

---
+++ src/app/models/User.js
@@ -0,0 +1,37 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    // Criptografa a senha, usamos o hook beforeSave para sempre que criar ou editar (atualizar)
+    // Pega o campo virtual, criptgrafa e joga o valor para a variavel de  hash
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  // compara os valores
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}
+
+export default User;
